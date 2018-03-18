ad64db1c0a4e95f92ce639a649fd421303a1a2d6
Keon Ye <jnuyeyizhou@gmail.com>
2018-03-18 19:33:07 +0800

    update the test.py script

--- test.py
+++ test.py
@@ -2,24 +2,46 @@
 # -*- coding: UTF-8 -*-
 # 文件名：test.py
 
-if True:
-  print "Answer: ",
-  print "True"
-else:
-  print "Answer: ",
-  # 没有严格缩进，在执行时会报错
-  print "False"
+# if True:
+#   print "Answer: ",
+#   print "True"
+# else:
+#   print "Answer: ",
+#   # 没有严格缩进，在执行时会报错
+#   print "False"
 
 
-raw_input("按下 enter 键退出，其他任意键显示...\n")
+# raw_input("按下 enter 键退出，其他任意键显示...\n")
 
 
-import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+# import sys; x = 'runoob'; sys.stdout.write(x + '\n')
 
-counter = 100 # 赋值整型变量
-miles = 1000.0 # 浮点型
-name = "John" # 字符串
+# counter = 100 # 赋值整型变量
+# miles = 1000.0 # 浮点型
+# name = "John" # 字符串
  
-print counter
-print miles
-print name
+# print counter
+# print miles
+# print 
+
+import requests
+
+url = "https://www.baidu.com"
+
+data = requests.get(url)
+
+print "=========================="
+print "Response Code: ", 
+print data
+print "=========================="
+
+print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+print data.text
+print "==============="
+code = data.encoding  
+print code
+print "==============="
+page_status = data.status_code  
+print page_status
+print "==============="
+print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
